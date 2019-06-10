1cb5c4f6f1593dcd545a806ef26097d5df848948
Andrew Bellucci <andrewbellucci@Andrews-MacBook-Pro-2.local>
2019-06-10 14:42:32 -0400

    vid 22 done

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About this App</h1>
+            <p>Small applet to search for Github Users</p>
+            <p>v1.0.0</p>
+        </React.Fragment>
+    )
+}
+
+export default About
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
@@ -10,12 +11,25 @@ export class Search extends Component {
             [event.target.name]: event.target.value
         })
     }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        setAlert: PropTypes.func.isRequired
+    }
+
     onSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.text)
+        if(this.state.text === ''){
+            this.props.setAlert('Please enter something', 'light')
+        }else{
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
+        
     }
 
     render() {
+        const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form className="form" onSubmit={this.onSubmit}>
@@ -23,10 +37,15 @@ export class Search extends Component {
                         type="text" 
                         name="text" 
                         placeholder="Search Users..."
+                        value={this.state.text}
                         onChange={this.onChange}
                     />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClear && (
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                )}
+                
             </div>
         )
     }
